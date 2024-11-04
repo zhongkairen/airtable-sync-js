54537a87960125d4f067f561c0769a9a0c98de58
Zhongkai Ren <zhongkai.ren@unity3d.com>
2024-11-05 00:14:41 +0200

    remove typess'

--- src/@types/graphql-request.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'graphql-request' {
-    export class GraphQLClient {
-        constructor(endpoint: string, options?: any);
-        request<T = any>(query: string, variables?: any): Promise<T>;
-    }
-}
